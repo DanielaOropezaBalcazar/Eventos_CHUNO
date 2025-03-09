28623ebbab72e7adf13a57bad1e0cce2967eb0c1
NataliaUrrutiaAgudo <150816023+natis25@users.noreply.github.com>
2025-03-09 01:04:03 -0400

    Add files via upload

--- Presentacion/eliminar.php
+++ Presentacion/eliminar.php
@@ -5,7 +5,7 @@ $controller = new CharlaController();
 $id = $_GET['id'] ?? null;
 
 if ($id && $controller->eliminar($id)) {
-    header("Location: index.php");
+    header("Location: charlas.php");
     exit();
 } else {
     echo "Error al eliminar la charla.";
